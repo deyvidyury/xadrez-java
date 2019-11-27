4056d7a5acf114c99f8c3b3711b7b78b6c703441
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-11-27 14:21:00 -0300

    placing pieces

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ * King
+ */
+public class King extends ChessPiece {
+
+  public King(Board board, Color color) {
+    super(board, color);
+  }
+
+  @Override
+  public String toString() {
+    return "K";
+  }
+
+}
